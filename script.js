bdb0d486d7e60a9ac2f09e57656ec7a871f98f3b
dragonvedb <dragonvedb@gmail.com>
2023-03-26 22:09:41 +0300

    Add a preliminary function for creating and rendering book cards

--- script.js
+++ script.js
@@ -18,14 +18,35 @@ function addToLibrary(name, author, pages, readStatus) {
   myLibrary.push(newBook);
 }
 
-/* addToLibrary("The Hobbit", "J.R.R. Tolkien", 259, false);
+const cardsContainer = document.querySelector("#cards-container");
+
+function createCard(book) {
+  function createElement(tag, parentElem, ...elemClasses) {
+    const newElement = document.createElement(tag);
+
+    for (const elemClass of elemClasses) {
+      newElement.classList.add(elemClass);
+    }
+
+    parentElem.appendChild(newElement);
+    return newElement;
+  }
+
+  const newCard = createElement("article", cardsContainer, "book-card");
+  const cardTitle = createElement("div", newCard, "book-title");
+  cardTitle.textContent = `${book.name} by ${book.author}`;
+  /* newCard.appendChild(cardTitle);
+  cardsContainer.appendChild(newCard); */
+}
+
+addToLibrary("The Hobbit", "J.R.R. Tolkien", 259, false);
 addToLibrary("Game of Thrones", "George R.R. Martin", 548, false);
 addToLibrary("City of Sol", "Eugene Rudashevsky", 305, true);
+
+createCard(myLibrary[0]);
 
 function allInfo() {
   for (const book of myLibrary) {
     console.log(book.info());
   }
 }
-
-allInfo(); */
