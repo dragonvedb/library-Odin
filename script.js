f6cda2ff7b3d3c95921f79e3c8c9de0b737aee66
dragonvedb <dragonvedb@gmail.com>
2023-03-27 17:56:35 +0300

    Finish the card constructor function, now adds text with correct info on the book

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const myLibrary = [];
 
-function Book(name, author, pages, readStatus) {
+function Book(name, author, date, chapters, pages, readStatus) {
   this.name = name;
   this.author = author;
+  this.date = date;
+  this.chapters = chapters;
   this.pages = pages;
   this.readStatus = readStatus;
 }
@@ -13,8 +15,8 @@ Book.prototype.info = function () {
   }.`;
 };
 
-function addToLibrary(name, author, pages, readStatus) {
-  const newBook = new Book(name, author, pages, readStatus);
+function addToLibrary(name, author, date, chapters, pages, readStatus) {
+  const newBook = new Book(name, author, date, chapters, pages, readStatus);
   myLibrary.push(newBook);
 }
 
@@ -41,17 +43,26 @@ function createCard(book) {
 
   const bookInfo = createElement("section", newCard, "book-info");
   const bookName = createElement("p", bookInfo, "book-name");
+  bookName.textContent = book.name;
   const bookAuthor = createElement("p", bookInfo, "book-author");
+  bookAuthor.textContent = `by ${book.author}`;
   const bookDate = createElement("p", bookInfo, "book-date");
+  bookDate.textContent = `Publication date: ${book.date}`;
   const bookLength = createElement("p", bookInfo, "book-length");
+  if (book.chapters > 1) {
+    bookLength.textContent = `${book.chapters} chapters || ${book.pages} pages`;
+  } else bookLength.textContent = `${book.pages} pages`;
 
   const cardButtonGroup = createElement("section", newCard, "card-buttons");
   const readBtn = createElement("button", cardButtonGroup, "read-btn");
+  readBtn.textContent = "Mark as read";
   const editBtn = createElement("button", cardButtonGroup, "edit-btn");
+  editBtn.textContent = "Edit card";
   const removeBtn = createElement("button", cardButtonGroup, "remove-btn");
+  removeBtn.textContent = "Remove";
 }
 
-addToLibrary("The Hobbit", "J.R.R. Tolkien", 259, false);
+addToLibrary("The Hobbit", "J.R.R. Tolkien", "1937-9-21", "25", "259", false);
 addToLibrary("Game of Thrones", "George R.R. Martin", 548, false);
 addToLibrary("City of Sol", "Eugene Rudashevsky", 305, true);
 
