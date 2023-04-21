d415ffa5fa4fdef30efacfe04c405765ff576685
dragonvedb <dragonvedb@gmail.com>
2023-04-21 13:28:57 +0300

    Rewrite using classes

--- script.js
+++ script.js
@@ -87,28 +87,152 @@ const startLibrary = [
   },
 ];
 
-function Book(
-  id,
-  name,
-  author,
-  date,
-  chapters,
-  pages,
-  coverColour,
-  textColour,
-  coverFont,
-  readStatus
-) {
-  this.id = id;
-  this.name = name;
-  this.author = author;
-  this.date = date;
-  this.chapters = Number(chapters);
-  this.pages = Number(pages);
-  this.coverColour = coverColour;
-  this.textColour = textColour;
-  this.coverFont = coverFont;
-  this.readStatus = !!readStatus;
+class Book {
+  constructor(
+    id,
+    name,
+    author,
+    date,
+    chapters,
+    pages,
+    coverColour,
+    textColour,
+    coverFont,
+    readStatus
+  ) {
+    this.id = id;
+    this.name = name;
+    this.author = author;
+    this.date = date;
+    this.chapters = Number(chapters);
+    this.pages = Number(pages);
+    this.coverColour = coverColour;
+    this.textColour = textColour;
+    this.coverFont = coverFont;
+    this.readStatus = !!readStatus;
+  }
+
+  createCard() {
+    function createElement(tag, parentElem, ...elemClasses) {
+      const newElement = document.createElement(tag);
+
+      for (const elemClass of elemClasses) {
+        newElement.classList.add(elemClass);
+      }
+
+      parentElem.appendChild(newElement);
+      return newElement;
+    }
+
+    let newCard;
+    if (document.querySelector(`[data-id="${this.id}"]`)) {
+      newCard = document.querySelector(`[data-id="${this.id}"]`);
+      newCard.innerHTML = "";
+    } else {
+      newCard = createElement("article", cardsContainer, "book-card");
+      newCard.setAttribute("data-id", this.id);
+      if (this.readStatus) newCard.classList.add("read");
+    }
+
+    const bookItself = createElement("section", newCard, "book-itself");
+    const bookBlock = createElement("div", bookItself, "book-block");
+    let blockWidth = 10 + Math.floor(this.pages / 25);
+    if (blockWidth > 50) blockWidth = 50;
+    bookBlock.setAttribute(
+      "style",
+      `height: ${blockWidth}px; border-color: ${this.coverColour}; border-right: 1px solid transparent`
+    );
+    const bookCover = createElement("div", bookItself, "book-cover");
+    const coverFontFamily = () => {
+      switch (this.coverFont) {
+        case "1":
+          return "'Libre Baskerville', serif";
+        case "2":
+          return "'Oswald', sans-serif";
+        case "3":
+          return "'Josefin Sans', sans-serif";
+        case "4":
+          return "'Macondo', cursive";
+        case "5":
+          return "'Amarante', cursive";
+        case "6":
+          return "'Charm', cursive";
+        case "7":
+          return "'Sue Ellen Francisco', cursive";
+        case "8":
+          return "'Rock Salt', cursive";
+        default:
+          return "'Libre Baskerville', serif";
+      }
+    };
+    const coverFontSize = () => {
+      if (this.name.length < 50) {
+        return 2.85 - this.name.length * 0.035;
+      }
+      return 1.1;
+    };
+    bookCover.setAttribute(
+      "style",
+      `background-color: ${this.coverColour}; color: ${
+        this.textColour
+      }; font-family: ${coverFontFamily()}; font-size: ${coverFontSize()}rem`
+    );
+    bookCover.textContent = this.name;
+
+    const bookInfo = createElement("section", newCard, "book-info");
+    const bookName = createElement("p", bookInfo, "book-name");
+    bookName.textContent = this.name;
+    const bookAuthor = createElement("p", bookInfo, "book-author");
+    bookAuthor.textContent = `by ${this.author}`;
+    const bookDate = createElement("p", bookInfo, "book-date");
+    bookDate.textContent = `Publication date: ${this.date}`;
+    const bookLength = createElement("p", bookInfo, "book-length");
+    if (this.chapters > 1) {
+      bookLength.textContent = `${Number(this.chapters)} chapters || ${Number(
+        this.pages
+      )} ${this.pages > 1 ? "pages" : "page"}`;
+    } else
+      bookLength.textContent = `${this.pages} ${
+        this.pages > 1 ? "pages" : "page"
+      }`;
+
+    const statusBtn = createElement("button", newCard, "status-button");
+    statusBtn.addEventListener("click", (e) => {
+      const parentCard = e.target.parentElement;
+      parentCard.classList.toggle("read");
+      if (this.readStatus === true) {
+        this.readStatus = false;
+      } else this.readStatus = true;
+    });
+
+    const cardButtonGroup = createElement("section", newCard, "card-buttons");
+    const editBtn = createElement("button", cardButtonGroup, "edit-btn");
+    editBtn.addEventListener("click", (e) => {
+      toggleForm();
+      document.getElementById("form-id").value = this.id;
+      document.getElementById("name-field").value = this.name;
+      document.getElementById("author-field").value = this.author;
+      document.getElementById("date-field").value = this.date;
+      document.getElementById("chapters-field").value = this.chapters;
+      document.getElementById("pages-field").value = this.pages;
+      document.getElementById("color-field").value = this.coverColour;
+      document.getElementById("text-color-field").value = this.textColour;
+      document.getElementById("font-field").value = this.coverFront;
+    });
+
+    const removeBtn = createElement("button", cardButtonGroup, "remove-btn");
+    removeBtn.addEventListener("click", (e) => {
+      document.querySelector(`[data-id="${this.id}"]`).remove();
+      myLibrary.splice(myLibrary.indexOf(this), 1);
+
+      for (let i = this.id; i < myLibrary.length; i++) {
+        const book = myLibrary[i];
+        const bookCard = document.querySelector(`[data-id="${book.id}"]`);
+        book.id = String(i);
+        bookCard.setAttribute("data-id", book.id);
+      }
+    });
+  }
 }
 
 const formContainer = document.getElementById("form-container");
@@ -149,128 +273,6 @@ const cancelButton = document.getElementById("cancel-btn");
 cancelButton.onclick = () => toggleForm();
 
 const cardsContainer = document.querySelector("#cards-container");
-
-Book.prototype.createCard = function () {
-  function createElement(tag, parentElem, ...elemClasses) {
-    const newElement = document.createElement(tag);
-
-    for (const elemClass of elemClasses) {
-      newElement.classList.add(elemClass);
-    }
-
-    parentElem.appendChild(newElement);
-    return newElement;
-  }
-
-  let newCard;
-  if (document.querySelector(`[data-id="${this.id}"]`)) {
-    newCard = document.querySelector(`[data-id="${this.id}"]`);
-    newCard.innerHTML = "";
-  } else {
-    newCard = createElement("article", cardsContainer, "book-card");
-    newCard.setAttribute("data-id", this.id);
-    if (this.readStatus) newCard.classList.add("read");
-  }
-
-  const bookItself = createElement("section", newCard, "book-itself");
-  const bookBlock = createElement("div", bookItself, "book-block");
-  let blockWidth = 10 + Math.floor(this.pages / 25);
-  if (blockWidth > 50) blockWidth = 50;
-  bookBlock.setAttribute(
-    "style",
-    `height: ${blockWidth}px; border-color: ${this.coverColour}; border-right: 1px solid transparent`
-  );
-  const bookCover = createElement("div", bookItself, "book-cover");
-  const coverFontFamily = () => {
-    switch (this.coverFont) {
-      case "1":
-        return "'Libre Baskerville', serif";
-      case "2":
-        return "'Oswald', sans-serif";
-      case "3":
-        return "'Josefin Sans', sans-serif";
-      case "4":
-        return "'Macondo', cursive";
-      case "5":
-        return "'Amarante', cursive";
-      case "6":
-        return "'Charm', cursive";
-      case "7":
-        return "'Sue Ellen Francisco', cursive";
-      case "8":
-        return "'Rock Salt', cursive";
-      default:
-        return "'Libre Baskerville', serif";
-    }
-  };
-  const coverFontSize = () => {
-    if (this.name.length < 50) {
-      return 2.85 - this.name.length * 0.035;
-    }
-    return 1.1;
-  };
-  bookCover.setAttribute(
-    "style",
-    `background-color: ${this.coverColour}; color: ${
-      this.textColour
-    }; font-family: ${coverFontFamily()}; font-size: ${coverFontSize()}rem`
-  );
-  bookCover.textContent = this.name;
-
-  const bookInfo = createElement("section", newCard, "book-info");
-  const bookName = createElement("p", bookInfo, "book-name");
-  bookName.textContent = this.name;
-  const bookAuthor = createElement("p", bookInfo, "book-author");
-  bookAuthor.textContent = `by ${this.author}`;
-  const bookDate = createElement("p", bookInfo, "book-date");
-  bookDate.textContent = `Publication date: ${this.date}`;
-  const bookLength = createElement("p", bookInfo, "book-length");
-  if (this.chapters > 1) {
-    bookLength.textContent = `${Number(this.chapters)} chapters || ${Number(
-      this.pages
-    )} ${this.pages > 1 ? "pages" : "page"}`;
-  } else
-    bookLength.textContent = `${this.pages} ${
-      this.pages > 1 ? "pages" : "page"
-    }`;
-
-  const statusBtn = createElement("button", newCard, "status-button");
-  statusBtn.addEventListener("click", (e) => {
-    const parentCard = e.target.parentElement;
-    parentCard.classList.toggle("read");
-    if (this.readStatus === true) {
-      this.readStatus = false;
-    } else this.readStatus = true;
-  });
-
-  const cardButtonGroup = createElement("section", newCard, "card-buttons");
-  const editBtn = createElement("button", cardButtonGroup, "edit-btn");
-  editBtn.addEventListener("click", (e) => {
-    toggleForm();
-    document.getElementById("form-id").value = this.id;
-    document.getElementById("name-field").value = this.name;
-    document.getElementById("author-field").value = this.author;
-    document.getElementById("date-field").value = this.date;
-    document.getElementById("chapters-field").value = this.chapters;
-    document.getElementById("pages-field").value = this.pages;
-    document.getElementById("color-field").value = this.coverColour;
-    document.getElementById("text-color-field").value = this.textColour;
-    document.getElementById("font-field").value = this.coverFront;
-  });
-
-  const removeBtn = createElement("button", cardButtonGroup, "remove-btn");
-  removeBtn.addEventListener("click", (e) => {
-    document.querySelector(`[data-id="${this.id}"]`).remove();
-    myLibrary.splice(myLibrary.indexOf(this), 1);
-
-    for (let i = this.id; i < myLibrary.length; i++) {
-      const book = myLibrary[i];
-      const bookCard = document.querySelector(`[data-id="${book.id}"]`);
-      book.id = String(i);
-      bookCard.setAttribute("data-id", book.id);
-    }
-  });
-};
 
 function displayLibrary(library) {
   for (const book of library) {
